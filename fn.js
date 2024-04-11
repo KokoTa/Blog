f3e15e75d60e8794a8e5b6c5bd9b1e48dc6be2b9
KokoTa <584847514@qq.com>
2024-04-11 13:53:22 +0800

    feat✨: 文档更新

--- fn.js
+++ fn.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: KokoTa
+ * @Date: 2024-03-28 09:36:02
+ * @LastEditTime: 2024-04-10 15:01:47
+ * @LastEditors: KokoTa
+ * @Description:
+ */
 const obj = {}, a = { x: 1 }, b = { x: 1 }
 obj[a] = 1
 obj[b] = 2
